83f06d0652822c0982e23802d2e9573e9fe746aa
Tobias Weber <t.weber@campus.lmu.de>
2024-01-30 17:21:59 +0100

    Updated benchmark script

--- scripts/01_model_benchmark.py
+++ scripts/01_model_benchmark.py
@@ -1,11 +1,11 @@
 import os
 
-from nnunetv2.inference.predict_from_raw_data import load_what_we_need
 import torch
-from torchinfo import summary
+from fvcore.nn import FlopCountAnalysis, flop_count_table, parameter_count
+from nnunetv2.inference.predict_from_raw_data import load_what_we_need
 
-from tuckercnn.utils import Timer
 from tuckercnn.tucker import DecompositionAgent
+from tuckercnn.utils import Timer, streamline_nnunet_architecture
 
 MODEL_OUTPUT_DIR = (
     '.totalsegmentator/nnunet/results/'
@@ -16,34 +16,67 @@ MODEL_OUTPUT_DIR = (
 TUCKER_ARGS = {
     'rank_mode': 'relative',
     'rank_factor': 1 / 3,
-    'rank_min': 16,
+    'rank_min': None,
     'decompose': False,
     'verbose': False,
+}
+
+BENCHMARK_ARGS = {
+    'patch_size': (1, 112, 112, 128),
+    'batch_size': 1,
+    'device': 'cuda',
+    'load_params': False,
+    'apply_tucker': True,
+    'autocast': False,
+    'compile': False,
+    'eval_passes': 10,
 }
 
 
 @torch.no_grad()
 def main() -> None:
+    # Load Network
+    # ----------------------------------------------------------------------------------
     mostly_useless_stuff = load_what_we_need(
         model_training_output_dir=os.path.join(os.environ['HOME'], MODEL_OUTPUT_DIR),
         use_folds=[0],
         checkpoint_name='checkpoint_final.pth',
     )
-
-    params = mostly_useless_stuff[0][0]
     network = mostly_useless_stuff[-2]
 
-    network.load_state_dict(params)
-    network = network.cuda()
+    if BENCHMARK_ARGS['load_params']:
+        params = mostly_useless_stuff[0][0]
+        network.load_state_dict(params)
+
+    if BENCHMARK_ARGS['apply_tucker']:
+        network = DecompositionAgent(tucker_args=TUCKER_ARGS)(network)
+
+    network = streamline_nnunet_architecture(network)
+    network = network.to(BENCHMARK_ARGS['device'])
     network.eval()
 
-    network = DecompositionAgent(tucker_args=TUCKER_ARGS)(network)
-    x = torch.rand(5, 1, 112, 112, 128).cuda()
+    # Count FLOPs
+    # ----------------------------------------------------------------------------------
+    x = torch.rand(
+        BENCHMARK_ARGS['batch_size'],
+        *BENCHMARK_ARGS['patch_size'],
+        device=BENCHMARK_ARGS['device'],
+    )
 
-    summary(network, (1, 1, 112, 112, 128))
+    flops = FlopCountAnalysis(network, x)
+    print(flop_count_table(flops))
+    print(f'Total Giga Flops: {flops.total() / 1000 ** 3:.3f}G')
+    print(f'Number of parameters: {parameter_count(network)[""] / 1e6:.3f}M')
 
-    with torch.autocast('cuda'):
-        for i in range(21):
+    # Measure execution time
+    # ----------------------------------------------------------------------------------
+    if BENCHMARK_ARGS['compile']:
+        network = torch.compile(network)
+
+    print('Measuring forward pass time ...')
+
+    with torch.autocast(BENCHMARK_ARGS['device'], enabled=BENCHMARK_ARGS['autocast']):
+        for i in range(BENCHMARK_ARGS['eval_passes'] + 1):
             with Timer():
                 network(x)
 
