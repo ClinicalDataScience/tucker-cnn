196a8b9f14a4e8a43a599fb9e2124f4a94181f95
Jakob Dexl <jakob.dexl@lmu.de>
2024-02-14 14:08:36 +0100

    rebased

--- scripts/05_finetune.py
+++ scripts/05_finetune.py
@@ -2,11 +2,26 @@ import nnunetv2
 from nnunetv2.run.run_training import run_training_entry
 from tuckercnn.monkey_patch_train import maybe_load_checkpoint
 from unittest.mock import patch
+import os
 
 if __name__ == '__main__':
-    # nnunetv2.run.run_training.maybe_load_checkpoint = maybe_load_checkpoint
-    # either
+    os.environ["nnUNet_preprocessed"] = "/data/core-rad/data/tucker/preprocessed"
+    os.environ["nnUNet_raw"] = "/data/core-rad/data/tucker/raw"
+    os.environ["nnUNet_results"] = "/data/core-rad/data/tucker/results"
+    # export nnUNet_preprocessed="/data/core-rad/data/tucker/preprocessed"
+    # export nnUNet_raw="/data/core-rad/data/tucker/raw"
+    # export nnUNet_results="/data/core-rad/data/tucker/results"
+    # export nnUNet_n_proc_DA=28
+    # setup nnUNetv2_plan_and_preprocess -d 001 -pl ExperimentPlanner -c 3d_fullres -np 2
+    # copy total seg plan to file and change ds, then
+    # nnUNetv2_preprocess -d 001 -plans_name /data/core-rad/data/tucker/preprocessed/Dataset001-torgan/nnUNetPlans -c 3d_fullres -np 28
+
+    nnunetv2.run.run_training.maybe_load_checkpoint = maybe_load_checkpoint
+
     run_args = ['', '300', '3d_fullres', '0', '-tr', 'nnUNetTrainerNoMirroring']
+    run_args = ['', '001', '3d_fullres', '0', '-tr', 'nnUNetTrainerNoMirroring', '-pretrained_weights',
+                '/data/core-rad/data/tucker/results/Dataset291_TotalSegmentator_part1_organs_1559subj/nnUNetTrainerNoMirroring__nnUNetPlans__3d_fullres/fold_0/checkpoint_final.pth']
+
     with patch("sys.argv", run_args):
         run_training_entry()
     # or run with sysargs like nnunet_train
--- tuckercnn/monkey_patch_train.py
+++ tuckercnn/monkey_patch_train.py
@@ -5,11 +5,21 @@ from copy import deepcopy
 from tuckercnn.tucker import DecompositionAgent
 from typing import Optional
 from dataclasses import dataclass
+import torch
+from torch.cuda.amp import GradScaler
+
+TUCKER_ARGS = {
+    'rank_mode': 'relative',
+    'rank_factor': 1 / 3,
+    'rank_min': None,
+    'decompose': True,
+    'verbose': True,
+}
 
 
 @dataclass
 class MonkeyManager:
-    tucker_args: Optional[dict] = None
+    tucker_args = TUCKER_ARGS
     apply_tucker = True
     inference_bs = 1
     ckpt_path = ''
@@ -17,50 +27,66 @@ class MonkeyManager:
     load_model = False
 
 
-def maybe_load_checkpoint(
-    nnunet_trainer: nnUNetTrainer,
-    continue_training: bool,
-    validation_only: bool,
-    pretrained_weights_file: str = None,
-):
+from torch.optim.lr_scheduler import _LRScheduler
+
+
+class PolyLRScheduler(_LRScheduler):
+    def __init__(self, optimizer, initial_lr: float, max_steps: int, exponent: float = 0.9, current_step: int = None):
+        self.optimizer = optimizer
+        self.initial_lr = initial_lr
+        self.max_steps = max_steps
+        self.exponent = exponent
+        self.ctr = 0
+        super().__init__(optimizer, current_step if current_step is not None else -1, False)
+
+    def step(self, current_step=None):
+        if current_step is None or current_step == -1:
+            current_step = self.ctr
+            self.ctr += 1
+
+        new_lr = self.initial_lr * (1 - current_step / self.max_steps) ** self.exponent
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = new_lr
+
+
+def configure_optimizers(nntrainer):
+    optimizer = torch.optim.SGD(nntrainer.network.parameters(), nntrainer.initial_lr,
+                                weight_decay=nntrainer.weight_decay,
+                                momentum=0.99, nesterov=True)
+    lr_scheduler = PolyLRScheduler(optimizer, nntrainer.initial_lr, nntrainer.num_epochs)
+    return optimizer, lr_scheduler
+
+
+def maybe_load_checkpoint(nnunet_trainer: nnUNetTrainer, continue_training: bool, validation_only: bool,
+                          pretrained_weights_file: str = None):
     if continue_training and pretrained_weights_file is not None:
-        raise RuntimeError(
-            'Cannot both continue a training AND load pretrained weights. Pretrained weights can only '
-            'be used at the beginning of the training.'
-        )
+        raise RuntimeError('Cannot both continue a training AND load pretrained weights. Pretrained weights can only '
+                           'be used at the beginning of the training.')
     if continue_training:
-        expected_checkpoint_file = join(
-            nnunet_trainer.output_folder, 'checkpoint_final.pth'
-        )
+        expected_checkpoint_file = join(nnunet_trainer.output_folder, 'checkpoint_final.pth')
         if not isfile(expected_checkpoint_file):
-            expected_checkpoint_file = join(
-                nnunet_trainer.output_folder, 'checkpoint_latest.pth'
-            )
+            expected_checkpoint_file = join(nnunet_trainer.output_folder, 'checkpoint_latest.pth')
         # special case where --c is used to run a previously aborted validation
         if not isfile(expected_checkpoint_file):
-            expected_checkpoint_file = join(
-                nnunet_trainer.output_folder, 'checkpoint_best.pth'
-            )
+            expected_checkpoint_file = join(nnunet_trainer.output_folder, 'checkpoint_best.pth')
         if not isfile(expected_checkpoint_file):
-            print(
-                f"WARNING: Cannot continue training because there seems to be no checkpoint available to "
-                f"continue from. Starting a new training..."
-            )
+            print(f"WARNING: Cannot continue training because there seems to be no checkpoint available to "
+                  f"continue from. Starting a new training...")
             expected_checkpoint_file = None
     elif validation_only:
-        expected_checkpoint_file = join(
-            nnunet_trainer.output_folder, 'checkpoint_final.pth'
-        )
+        expected_checkpoint_file = join(nnunet_trainer.output_folder, 'checkpoint_final.pth')
         if not isfile(expected_checkpoint_file):
-            raise RuntimeError(
-                f"Cannot run validation because the training is not finished yet!"
-            )
+            raise RuntimeError(f"Cannot run validation because the training is not finished yet!")
     else:
         if pretrained_weights_file is not None:
             if not nnunet_trainer.was_initialized:
                 nnunet_trainer.initialize()
 
             if MonkeyManager.apply_tucker:
+                load_pretrained_weights(nnunet_trainer.network, pretrained_weights_file, verbose=True)
+                # nnunet_trainer.load_checkpoint(pretrained_weights_file)
+                print("Start decomposition")
+                print(nnunet_trainer.network)
                 network = DecompositionAgent(
                     tucker_args=MonkeyManager.tucker_args,
                     ckpt_path=MonkeyManager.ckpt_path,
@@ -68,10 +94,14 @@ def maybe_load_checkpoint(
                     load_model=MonkeyManager.load_model,
                 )(deepcopy(nnunet_trainer.network))
                 nnunet_trainer.network = network
+                print("Start decomposition")
+                print(nnunet_trainer.network)
+                nnunet_trainer.grad_scaler = GradScaler()  # if self.device.type == 'cuda' else None
+                nnunet_trainer.optimizer, nnunet_trainer.lr_scheduler = configure_optimizers(nnunet_trainer)
+                nnunet_trainer.network.train()
             else:
-                load_pretrained_weights(
-                    nnunet_trainer.network, pretrained_weights_file, verbose=True
-                )
+                load_pretrained_weights(nnunet_trainer.network, pretrained_weights_file, verbose=True)
+            # nnunet_trainer.load_checkpoint(pretrained_weights_file)
         expected_checkpoint_file = None
 
     if expected_checkpoint_file is not None:
